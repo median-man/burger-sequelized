42d0aadaaee5e9f297e328930cbe8e59a54224c7
John Desrosiers <johndrunner@yahoo.com>
2017-10-07 13:22:10 -0700

    more testing configuration

--- karma.conf.js
+++ karma.conf.js
@@ -10,12 +10,12 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
+    frameworks: ['mocha', 'chai', 'browserify'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      './test/*.js'
+		'./test/*_karma.js'		
     ],
 
 
@@ -27,6 +27,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+		'./test/*.js': ['browserify']
     },
 
 
