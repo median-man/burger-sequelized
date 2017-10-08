9aea5671322d85b15da90db9bb33dc9e06c62188
John Desrosiers <johndrunner@yahoo.com>
2017-10-08 14:53:33 -0700

    remove unused dependencies

--- test/models_test.js
+++ test/models_test.js
@@ -3,13 +3,9 @@ process.env.NODE_ENV = process.argv[2];
 // dependencies
 const chai = require('chai');
 const { expect } = chai;
-const chaiHttp = require('chai-http');
-chai.use(chaiHttp);
 
 // application modules
 const db = require('../models');
-const seed = require('./seed.js');
-
 
 // test the database connection
 describe('db', function() {
